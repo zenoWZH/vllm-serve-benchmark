be4aef895983fa3502689988fd17fb0b90a3f2cf
zenoWZH <wangzhouhao94@gmail.com>
2024-10-05 13:46:56 -0700

    32 pods performance bug/drop

--- parallel_ha.py
+++ parallel_ha.py
@@ -4,16 +4,16 @@ from multiprocessing import Pool
 from backend_request_func import remove_prefix
 
 def run_benchmark(url):
-    url[0] = remove_prefix(url[0], prefix="http://")
+    url[0] = "https://" + remove_prefix(url[0], prefix="http://")
     cmd = f"export OPENAI_API_KEY=token-abc123 && python3 benchmark_serving.py \
             --backend vllm --model meta-llama/Meta-Llama-3-8B-Instruct \
             --dataset-name sharegpt --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json \
-            --num-prompts 20480 --host {url[0]} --port {url[1]} --request_rate=3.5"
+            --num-prompts 20480 --base-url {url[0]} --request_rate=3"
     result = subprocess.call(cmd, shell=True)
     return result
 
 if __name__ == '__main__':
-    input_csv = pd.read_csv("API_urls_nodeport.csv")[:1]
+    input_csv = pd.read_csv("API_urls.csv")
     url_list = input_csv[['ing','port']].values.tolist()
     results = []
     with Pool(processes=len(url_list)) as pool:
@@ -25,4 +25,3 @@ if __name__ == '__main__':
 #            for r in res:
 #                f.write(str(r))
 #            f.write("================================")
-
